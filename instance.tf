d9252b702e0649fecf8028d7cf4e90798475da4f
Julio Arana Jr <julio.arana@cloudreach.com>
2022-10-13 15:20:54 +0200

    Creating Instance file Terraform

---
+++ instance.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "tf_ring_instance" {
+  subnet_id              = data.aws_subnet.tf_data_subnet.id
+  instance_type          = var.instance_type
+  ami                    = data.aws_ami.tf_data_ami_ubuntu.id
+  vpc_security_group_ids = [aws_security_group.tf_ring_servers_sg.id]
+  key_name               = var.keypair
+  tags = {
+    Name = "tf-ring-instance"
+    Project = "ring-number-challenge-team-1"
+  }
+}
