f8c25283028b78fc892a3913ee20148357bfea98
Julio Arana Jr <julio.arana@cloudreach.com>
2022-10-17 19:58:36 +0200

    modified eip.tf

--- eip.tf
+++ eip.tf
@@ -1,8 +1,4 @@
 resource "aws_eip_association" "eip_assoc" {
   instance_id   = aws_instance.tf_ring_instance.id
-  allocation_id = aws_eip.ring.id
-}
-
-resource "aws_eip" "ring" {
   vpc = true
 }
