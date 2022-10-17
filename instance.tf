184930db768bb57b8b7dc663de4f5918b0a930a9
Julio Arana Jr <julio.arana@cloudreach.com>
2022-10-17 19:58:28 +0200

    Update Elastic IP tf File

--- instance.tf
+++ instance.tf
@@ -3,6 +3,8 @@ resource "aws_instance" "tf_ring_instance" {
   instance_type          = var.instance_type
   ami                    = data.aws_ami.tf_data_ami_ubuntu.id
   vpc_security_group_ids = [aws_security_group.tf_ring_servers_sg.id]
+  associate_public_ip_address = true
+  source_dest_check = false
   key_name               = var.keypair
   user_data              = templatefile("${path.module}/user-data.sh.tftpl", {
     github-access-token-placeholder = jsondecode(nonsensitive(data.aws_secretsmanager_secret_version.tf_ring_number_challenge_github_access_token_secret_version.secret_string))["github-access-token"],
