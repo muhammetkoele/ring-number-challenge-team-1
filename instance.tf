362b1c798967b74cba2d85bc177b047826cc73e0
Julio Arana Jr <julio.arana@cloudreach.com>
2022-10-18 11:20:29 +0200

    update instance tf

--- instance.tf
+++ instance.tf
@@ -3,8 +3,6 @@ resource "aws_instance" "tf_ring_instance" {
   instance_type          = var.instance_type
   ami                    = data.aws_ami.tf_data_ami_ubuntu.id
   vpc_security_group_ids = [aws_security_group.tf_ring_servers_sg.id]
-  associate_public_ip_address = true
-  source_dest_check = false
   key_name               = var.keypair
   user_data              = templatefile("${path.module}/user-data.sh.tftpl", {
     github-access-token-placeholder = jsondecode(nonsensitive(data.aws_secretsmanager_secret_version.tf_ring_number_challenge_github_access_token_secret_version.secret_string))["github-access-token"],
@@ -12,7 +10,6 @@ resource "aws_instance" "tf_ring_instance" {
     github-user-name-placeholder = var.github_user_name,
     github-user-email-placeholder = var.github_user_email
   })
-
   tags = {
     Name = "tf-ring-instance"
     Project = "ring-number-challenge-team-1"
