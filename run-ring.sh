dc78d5d15542253cb836f52300cb78fae6e1f392
jemiyin <jemila.daud@cloudreach.com>
2022-10-20 12:46:53 +0200

    adding ringrun file

--- run-ring.sh
+++ run-ring.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-cd ~/src/talent-academy/challenge_labs/ring-number-challenge-team-1/
+cd ~/src/Talent-Academy/ring-number-challenge-team-1
+
 git checkout jemila
 git pull
-ip1=`sed '1q;d' < ~/src/talent-academy/challenge_labs/ring-number-challenge-team-1/instance-ip.txt`
+ip1=`sed '1q;d' < ~/src/Talent-Academy/ring-number-challenge-team-1/instance-ip.txt`
 git checkout julioaranajr
 git pull
-ip2=`sed '1q;d' < ~/src/talent-academy/challenge_labs/ring-number-challenge-team-1/instance-ip.txt`
+ip2=`sed '1q;d' < ~/src/Talent-Academy/ring-number-challenge-team-1/instance-ip.txt`
 git checkout leticia
 git pull
-ip3=`sed '1q;d' < ~/src/talent-academy/challenge_labs/ring-number-challenge-team-1/instance-ip.txt`
+ip3=`sed '1q;d' < ~/src/Talent-Academy/ring-number-challenge-team-1/instance-ip.txt`
 git checkout muhammet
 git pull
-ip4=`sed '1q;d' < ~/src/talent-academy/challenge_labs/ring-number-challenge-team-1/instance-ip.txt`
+ip4=`sed '1q;d' < ~/src/Talent-Academy/ring-number-challenge-team-1/instance-ip.txt`
 git checkout rima
 git pull
-ip5=`sed '1q;d' < ~/src/talent-academy/challenge_labs/ring-number-challenge-team-1/instance-ip.txt`
-git checkout leticia
+ip5=`sed '1q;d' < ~/src/Talent-Academy/ring-number-challenge-team-1/instance-ip.txt`
+git checkout jemila
+
 printf '[servers_jemila]
 %s
 [servers_julio]
@@ -35,6 +37,7 @@ servers_muhammet
 servers_rima
 [servers:vars]
 ansible_user=ubuntu
-ansible_ssh_private_key_file=~/.ssh/ring-challenge-team-1' "$ip1" "$ip2" "$ip3" "$ip4" "$ip5"> ~//src/talent-academy/challenge_labs/ring-number-challenge-team-1/configuration/inventory.hosts
+ansible_ssh_private_key_file=~/.ssh/ring-challenge-team-1' "$ip1" "$ip2" "$ip3" "$ip4" "$ip5"> ~/src/Talent-Academy/ring-number-challenge-team-1/configuration/inventory.hosts
+
 cd configuration/
 ansible-playbook main-playbook.yaml
